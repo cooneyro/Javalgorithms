81d2597c8be43993e5c22c2b115761ccb2279f6d
Rob <roberttcooney@gmail.com>
2017-03-28 19:29:04 +0100

    adding

--- LinkedLists/src/SumOfLists.java
+++ LinkedLists/src/SumOfLists.java
@@ -3,61 +3,58 @@
 public class SumOfLists extends Node {
 
     /**
-     *
-     * @param firstList Head of first list to be added (least significant digit first)
+     * @param firstList  Head of first list to be added (least significant digit first)
      * @param secondList Head of second list to be added
      * @return sum of lists (least significant digit first)
      */
-    public Node SumListsRevOrder(Node firstList, Node secondList){
+    public Node SumListsRevOrder(Node firstList, Node secondList) {
         Node first = firstList, second = secondList;
         Node newList = null;
-        boolean carry =false;
+        boolean carry = false;
         int result;
 
-        while(first!=null && second!=null){
-            result = (first.getData() + second.getData() + (carry?1:0));
-            if(result>=10){
+        while (first != null && second != null) {
+            result = (first.getData() + second.getData() + (carry ? 1 : 0));
+            if (result >= 10) {
                 carry = true;
                 result %= 10;
-            }
-            else{
+            } else {
                 carry = false;
             }
 
-            if(newList==null){
+            if (newList == null) {
                 newList = new Node(result);
-            }
-            else{
+            } else {
                 newList.addToEnd(result);
             }
         }
 
-        if(first!=null){
-            while(first!=null){
-                result = (first.getData() + (carry?1:0));
-                if(result>=10){
+        if (first != null) {
+            while (first != null) {
+                result = (first.getData() + (carry ? 1 : 0));
+                if (result >= 10) {
                     result %= 10;
                 }
             }
-        }
-        else if(second!=null){
-            while(second!=null){
-                result = (second.getData() + (carry?1:0));
-                if(result>=10){
+        } else if (second != null) {
+            while (second != null) {
+                result = (second.getData() + (carry ? 1 : 0));
+                if (result >= 10) {
                     result %= 10;
                     carry = true;
-                }
-                else{
+                } else {
                     carry = false;
                 }
             }
         }
 
 
-        if(carry){
-                newList.addToEnd(1);  //if both lists are same length and carry left over after
-                                            //traversing, add carry digit to end of sum list
-            }
+        if (carry) {
+            newList.addToEnd(1);  //if both lists are same length and carry left over after
+            //traversing, add carry digit to end of sum list
+        }
 
+
+        return newList;
     }
 }
